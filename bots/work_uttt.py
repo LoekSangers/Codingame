63e1bac48dfe38af223749e2c3ebe0059d7846c0
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2022-04-08 19:10:02 +0000

    Copied rust implementation of uttt.

---
+++ bots/work_uttt.py
@@ -0,0 +1,362 @@
+"""My implementation for Codingame Ultimate TicTacToe."""
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import datetime
+from math import log
+from random import choice, shuffle
+
+class MonteCarloNode(ABC):
+    """Base class for a Monte Carlo Tree Search state"""
+
+    C = 1.4
+
+    def __init__(self, parent:MonteCarloNode = None) -> None:
+        self.visited_count = 0
+        self._wins_score = 0
+        self._loses_score = 0
+        self.parent = parent
+        self.unvisited_actions = []
+        self.children = {}
+
+    @property
+    def score(self) -> int:
+        """Return the score of this Node."""
+        return self._wins_score - self._loses_score
+
+    @property
+    def utc(self) -> float:
+        """Return the UTC value of this node.
+        Based on: https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
+        Section Exploration and exploitation.
+
+        """
+        return (self.score/self.visited_count) \
+            + self.C * (log(self.parent.visited_count)/self.visited_count)**.5
+
+    @property
+    def best_child(self) -> MonteCarloNode:
+        """Select the node with currently the highest utc value."""
+        return self.children[max(self.children, key=lambda key: self.children[key].utc)]
+
+    @property
+    @abstractmethod
+    def is_terminal(self):
+        """Returns whether this node is terminal."""
+
+    def select_leaf(self) -> MonteCarloNode:
+        """Select a Leaf Node for the next iterations.
+        Note: if fully expanded is recursive!
+
+        """
+        if not self.children:
+            self.expand()
+            shuffle(self.unvisited_actions)
+
+        if self.unvisited_actions:
+            return self.children[self.unvisited_actions.pop()]
+        return self.best_child.select_leaf()
+
+    @abstractmethod
+    def expand(self):
+        """Create all Child Nodes."""
+
+    @abstractmethod
+    def simulate(self) -> bool:
+        """Complete one playout.
+        Returns True if the starting player has won the playout.
+
+        """
+
+    def backpropagate(self, has_won:bool) -> None:
+        """Use the result of the simulation to update information
+        in the Nodes on the from the Child Node to the root Node.
+
+        """
+        self.visited_count += 1
+        if has_won:
+            self._wins_score += 1
+        else:
+            self._loses_score += 1
+        if self.parent:
+            self.parent.backpropagate(not has_won)
+
+    def run(self, run_time: float) -> None:
+        """Run simulations until we run out of run_time"""
+        #iter = 0
+        begin = datetime.datetime.now()
+        while (datetime.datetime.now() - begin).total_seconds() < run_time:
+            self.select_leaf().simulate()
+        #    iter += 1
+        #print(str(iter))
+
+class Action:
+    """Class for TicTacToe actions."""
+    def __init__(self, x_coord: int, y_coord: int, player: int) -> None:
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.player = player
+
+    def __eq__(self, obj: Action):
+        return self.player == obj.player \
+            and self.x_coord == obj.x_coord \
+            and self.y_coord == obj.y_coord
+
+    def tuple(self):
+        """Return action as tuple."""
+        return (self.player, self.x_coord, self.y_coord)
+
+
+
+class NormalBoard:
+    """Class for normal TicTacToe board."""
+    def __init__(self, normal_board: NormalBoard = None) -> None:
+        if normal_board is None:
+            self._grid = [0 for _ in range(9)]
+            self._moves_count = 0
+            self.local_winner = 0
+        else:
+            self._grid = normal_board._grid[:]
+            self._moves_count = normal_board._moves_count
+            self.local_winner = normal_board.local_winner
+
+
+    def get_valid_actions(self, current_player:int, base_x:int = 0, base_y:int = 0) -> list[Action]:
+        """List of valid moves on this board."""
+        if self.local_winner != 0:
+            return []
+
+        valid_actions = []
+        for y_coord in range(3):
+            for x_coord in range(3):
+                if self._grid[y_coord*3 + x_coord] == 0:
+                    valid_actions.append(Action(base_x + x_coord, base_y + y_coord, current_player))
+        return valid_actions
+
+    def play(self, move: Action, *, localize:bool = False) -> int:
+        """Play a given move on this board."""
+        x_coord = move.x_coord if not localize else move.x_coord % 3
+        y_coord = move.y_coord if not localize else move.y_coord % 3
+        if x_coord < 0 or x_coord >= 3 \
+                or y_coord < 0 or y_coord >= 3 \
+                or self._grid[y_coord*3 + x_coord] != 0:
+            raise InvalidMoveError(x_coord, y_coord, self._grid)
+
+        self._grid[y_coord*3 + x_coord] = move.player
+        self._moves_count += 1
+
+        return self._update_winner()
+
+    def _update_winner(self) -> int:
+        """Return the id of the player that has won.
+        Return -1 if the board has tied
+        Return 0 otherwise
+        """
+        for index in range(3):
+            #rows
+            if self._grid[index*3 + 0] > 0 \
+                    and self._grid[index*3 + 0] == self._grid[index*3 + 1] \
+                    and self._grid[index*3 + 0] == self._grid[index*3 + 2]:
+                self.local_winner = self._grid[index*3 + 0]
+                return self.local_winner
+
+            #cols
+            if self._grid[0 + index] > 0 \
+                    and self._grid[0 + index] == self._grid[3 + index] \
+                    and self._grid[0 + index] == self._grid[6 + index]:
+                self.local_winner = self._grid[0 + index]
+                return self.local_winner
+
+        #diagonals
+        if self._grid[0] > 0 \
+                and self._grid[0] == self._grid[4] \
+                and self._grid[0] == self._grid[8]:
+            self.local_winner = self._grid[0]
+            return self.local_winner
+        if self._grid[2] > 0 \
+                and self._grid[2] == self._grid[4] \
+                and self._grid[2] == self._grid[6]:
+            self.local_winner = self._grid[2]
+            return self.local_winner
+
+        if self._moves_count > 8:
+            self.local_winner = -1
+            return self.local_winner
+        return 0
+
+class UltimateBoard(MonteCarloNode):
+    """Class for Ultimate TicTacToe board."""
+    def __init__(self, *, last_move: Action = None, parent: UltimateBoard = None) -> None:
+        super().__init__(parent)
+        if parent is None:
+            self._grid = [NormalBoard() for _ in range(9)]
+            self._done_board_count = 0
+        else:
+            self._grid = [NormalBoard(normal_board) for normal_board in parent._grid]
+            self._done_board_count = parent._done_board_count
+
+        self.last_move = last_move
+        self.local_winner = 0
+
+    def get_valid_actions(self) -> list[Action]:
+        """List of valid moves on this board."""
+        valid_actions = []
+        if self.last_move is not None:
+            x_grid = self.last_move.x_coord % 3
+            y_grid = self.last_move.y_coord % 3
+            board = self._grid[y_grid*3 + x_grid]
+            if board.local_winner == 0:
+                valid_actions = board.get_valid_actions(
+                    1 if self.last_move.player != 1 else 2,
+                    x_grid*3,
+                    y_grid*3
+                )
+
+        if not valid_actions:
+            position: int
+            board: NormalBoard
+            for position, board in enumerate(self._grid):
+                if board.local_winner == 0:
+                    valid_actions.extend(
+                        board.get_valid_actions(
+                            1 if self.last_move.player != 1 else 2,
+                            (position%3)*3,
+                            (position//3)*3
+                        )
+                    )
+
+        return valid_actions
+
+    def play(self, move: Action) -> None:
+        """Play a given move on this board."""
+        x_grid = move.x_coord // 3
+        y_grid = move.y_coord // 3
+
+        winner = self._grid[y_grid*3 + x_grid].play(move, localize = True)
+
+        if winner != 0:
+            #someone won last board, so check whether game done
+            self._done_board_count += 1
+            self._calc_winner()
+
+        self.last_move = move
+
+    def _calc_winner(self) -> int:
+        """Return the id of the player that has won.
+        Return -1 if the board has tied
+        Return 0 otherwise
+        """
+        for index in range(3):
+            #rows
+            if self._grid[index*3 + 0].local_winner > 0 \
+            and self._grid[index*3 + 0].local_winner == self._grid[index*3 + 1].local_winner \
+            and self._grid[index*3 + 0].local_winner == self._grid[index*3 + 2].local_winner:
+                self.local_winner = self._grid[index*3 + 0].local_winner
+                return self.local_winner
+
+            #cols
+            if self._grid[0 + index].local_winner > 0 \
+            and self._grid[0 + index].local_winner == self._grid[3 + index].local_winner \
+            and self._grid[0 + index].local_winner == self._grid[6 + index].local_winner:
+                self.local_winner = self._grid[0 + index].local_winner
+                return self.local_winner
+
+        #diagonals
+        if self._grid[0].local_winner > 0 \
+                and self._grid[0].local_winner == self._grid[4].local_winner \
+                and self._grid[0].local_winner == self._grid[8].local_winner:
+            self.local_winner = self._grid[0].local_winner
+            return self.local_winner
+        if self._grid[6 + 0].local_winner > 0 \
+                and self._grid[6 + 0].local_winner == self._grid[4].local_winner \
+                and self._grid[6+ 0].local_winner == self._grid[2].local_winner:
+            self.local_winner = self._grid[6].local_winner
+            return self.local_winner
+
+        if self._done_board_count > 8:
+            self.local_winner = -1
+            return -1
+        return 0
+
+    @property
+    def is_terminal(self):
+        return self.local_winner != 0
+
+    def expand(self):
+        actions = self.get_valid_actions()
+
+        for action in actions:
+            child_node = UltimateBoard(parent=self)
+            child_node.play(action)
+            tup = action.tuple()
+            self.unvisited_actions.append(tup)
+            self.children[tup] = child_node
+
+    def simulate(self):
+        simulation_board = UltimateBoard(parent=self)
+        simulation_board.last_move = self.last_move
+        while not simulation_board.is_terminal:
+            try:
+                simulation_board.play(choice(simulation_board.get_valid_actions()))
+            except:
+                print(simulation_board.get_valid_actions())
+                for nb in self._grid:
+                    print(nb._grid)
+
+        self.backpropagate(simulation_board.local_winner not in [-1, self.last_move.player])
+
+    def print_move(self):
+        """Print the best move to console.
+        Note: They expect first the y coord then the x coord.
+        """
+        
+        print(str(self.last_move.y_coord) + " " + str(self.last_move.x_coord))
+
+
+# define Python user-defined exceptions
+class InvalidMoveError(Exception):
+    """Exception raised for Invalid moves."""
+
+    def __init__(self, x_coord: int, y_coord: int, board: NormalBoard) -> None:
+        self.message = f"Cannot move to {x_coord}, {y_coord} on board {board}"
+        super().__init__(self.message)
+
+def main():
+    """Main"""
+    #first turn
+    opponent_row, opponent_col = [int(i) for i in input().split()]
+    valid_action_count = int(input())
+    for _ in range(valid_action_count):
+        _, valid_action_count = [int(j) for j in input().split()]
+
+    root = UltimateBoard()
+
+    if opponent_row == -1:# I start in the middle for no apparent reason
+        root.play(Action(4, 4, 1))
+        root.run(.90)
+        print("4 4")
+    else:
+        act = Action(opponent_col, opponent_row, 2)
+        root.play(act)
+        root.run(.90)
+        root = root.best_child
+        root.parent = None
+        root.print_move()
+
+
+    #game loop
+    while True:
+        opponent_row, opponent_col = [int(i) for i in input().split()]
+        valid_action_count = int(input())
+        for _ in range(valid_action_count):
+            _, _ = [int(j) for j in input().split()]
+
+        if not root.children:
+            root.expand()
+
+        root = root.children[(2, opponent_col, opponent_row)]
+        root.run(.06)
+        root = root.best_child
+        root.parent = None
+        root.print_move()
+
+main()
---
+++ rust/uttt/src/main.rs
@@ -0,0 +1,458 @@
+use std::io;
+
+use std::vec::Vec;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+// based on: https://github.com/nelhage/ultimattt/blob/master/src/lib/game.rs
+// Removed SIMD as this is not included in safe rust which is needed for Codingame
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Player {
+    X,
+    O,
+}
+
+impl Player {
+    pub fn other(&self) -> Player {
+        match self {
+            Player::X => Player::O,
+            Player::O => Player::X,
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Player::X => "X",
+            Player::O => "O",
+        }
+    }
+
+    pub fn as_bit(&self) -> usize {
+        match self {
+            Player::X => 0,
+            Player::O => 1,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CellState {
+    Empty,
+    Played(Player),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum GameState {
+    InPlay,
+    Drawn,
+    Won(Player),
+}
+
+impl GameState {
+    pub fn terminal(&self) -> bool {
+        match self {
+            GameState::Drawn | GameState::Won(_) => true,
+            _ => false,
+        }
+    }
+}
+
+pub(in crate) const WIN_MASKS: &[u32] = &[
+    0b000_000_111,
+    0b000_111_000,
+    0b111_000_000,
+    0b001_001_001,
+    0b010_010_010,
+    0b100_100_100,
+    0b001_010_100,
+    0b100_010_001,
+];
+
+const BOARD_MASK: u32 = 0b111_111_111;
+
+#[derive(Clone, Debug)]
+pub struct LocalBoard([CellState; 9]);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Row {
+    // These are each a packed [u9; 3] containing a bitmask for the
+    // respective player's states. The low bits store index 0.
+    x: u32,
+    o: u32,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(in crate) struct LocalBoards {
+    rows: [Row; 3],
+}
+
+impl Default for LocalBoards {
+    fn default() -> Self {
+        LocalBoards {
+            rows: [Row { x: 0, o: 0 }, Row { x: 0, o: 0 }, Row { x: 0, o: 0 }],
+        }
+    }
+}
+
+impl LocalBoards {
+    fn at(&self, global: usize, local: usize) -> CellState {
+        let row = &self.rows[global / 3];
+        let idx = (global % 3) * 9 + local;
+        if (row.x >> idx) & 1 == 1 {
+            return CellState::Played(Player::X);
+        }
+        if (row.o >> idx) & 1 == 1 {
+            return CellState::Played(Player::O);
+        }
+        return CellState::Empty;
+    }
+
+    fn set(&mut self, global: usize, local: usize, who: Player) {
+        let mut row = &mut self.rows[global / 3];
+        let idx = (global % 3) * 9 + local;
+        let bit = 1 << idx;
+        match who {
+            Player::X => {
+                row.x |= bit;
+            }
+            Player::O => {
+                row.o |= bit;
+            }
+        }
+    }
+
+    pub(in crate) fn mask(&self, global: usize) -> u32 {
+        let row = &self.rows[global / 3];
+        (row.o | row.x) >> (9 * (global % 3)) & BOARD_MASK
+    }
+
+    fn check_winner(&self, global: usize, player: Player) -> GameState {
+        let row = &self.rows[global / 3];
+        let shift = 9 * (global % 3);
+        let mask = match player {
+            Player::X => row.x >> shift,
+            Player::O => row.o >> shift,
+        };
+
+        if WIN_MASKS.iter().any(|&win_mask| mask & win_mask == win_mask) {
+            return GameState::Won(player);
+        }
+
+        if ((row.x | row.o) >> shift) & BOARD_MASK == BOARD_MASK {
+            GameState::Drawn
+        } else {
+            GameState::InPlay
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(in crate) struct GlobalStates {
+    // LSB first; drawn if both x&y
+    x: u16,
+    o: u16,
+}
+
+impl Default for GlobalStates {
+    fn default() -> Self {
+        GlobalStates { x: 0, o: 0 }
+    }
+}
+
+impl GlobalStates {
+    pub(in crate) fn xbits(&self) -> u32 {
+        (self.x & !self.o) as u32
+    }
+    pub(in crate) fn obits(&self) -> u32 {
+        (self.o & !self.x) as u32
+    }
+    pub(in crate) fn drawbits(&self) -> u32 {
+        (self.x & self.o) as u32
+    }
+    pub(in crate) fn donebits(&self) -> u32 {
+        (self.x | self.o) as u32
+    }
+    pub(in crate) fn playerbits(&self, player: Player) -> u32 {
+        match player {
+            Player::X => self.xbits(),
+            Player::O => self.obits(),
+        }
+    }
+    pub(in crate) fn in_play(&self, board: usize) -> bool {
+        (self.donebits() & 1 << board) == 0
+    }
+
+    fn check_winner(&self, player: Player) -> GameState {
+        let mask = self.playerbits(player);
+        if WIN_MASKS.iter().any(|&win_mask| mask & win_mask == win_mask) {
+            return GameState::Won(player);
+        }
+        
+        if self.donebits() == BOARD_MASK {
+            GameState::Drawn
+        } else {
+            GameState::InPlay
+        }
+    }
+
+    fn at(&self, board: usize) -> GameState {
+        let bit = 1 << board;
+        if self.xbits() & bit != 0 {
+            GameState::Won(Player::X)
+        } else if self.obits() & bit != 0 {
+            GameState::Won(Player::O)
+        } else if self.drawbits() & bit != 0 {
+            GameState::Drawn
+        } else {
+            GameState::InPlay
+        }
+    }
+
+    fn set(&mut self, board: usize, state: GameState) {
+        let bit = 1_u16 << board;
+        match state {
+            GameState::Drawn => {
+                self.x |= bit;
+                self.o |= bit;
+            }
+            GameState::InPlay => {
+                return;
+            }
+            GameState::Won(Player::X) => self.x |= bit,
+            GameState::Won(Player::O) => self.o |= bit,
+        };
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct Move {
+    bits: u8,
+}
+
+impl Move {
+    pub fn from_coords(global: usize, local: usize) -> Self {
+        return Self {
+            bits: (global << 4 | local) as u8,
+        };
+    }
+
+    pub fn global(self) -> usize {
+        (self.bits >> 4) as usize
+    }
+
+    pub fn local(self) -> usize {
+        (self.bits & 0x0f) as usize
+    }
+
+    pub fn none() -> Self {
+        Move { bits: 0xff }
+    }
+
+    pub fn is_none(self) -> bool {
+        self.bits == 0xff
+    }
+
+    pub fn is_some(self) -> bool {
+        !self.is_none()
+    }
+
+    pub fn bits(self) -> u8 {
+        self.bits
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum MoveError {
+    WrongBoard,
+    WonBoard,
+    OutOfBounds,
+    NotEmpty,
+    GameOver,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Game {
+    pub(in crate) next_player: Player,
+    pub(in crate) next_board: Option<u8>,
+    pub(in crate) local_boards: LocalBoards,
+    pub(in crate) global_states: GlobalStates,
+    pub(in crate) game_state: GameState,
+    hash: u64,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            next_player: Player::X,
+            next_board: None,
+            local_boards: Default::default(),
+            global_states: Default::default(),
+            game_state: GameState::InPlay,
+            hash: 0,
+        }
+    }
+
+    pub fn make_move(&self, m: Move) -> Result<Game, MoveError> {
+        let mut out = self.clone();
+        out.inplace_move(m).map(|_| out)
+    }
+
+    pub fn inplace_move(&mut self, m: Move) -> Result<(), MoveError> {
+        if m.global() >= 9 || m.local() >= 9 {
+            return Err(MoveError::OutOfBounds);
+        }
+        match self.game_state {
+            GameState::InPlay => (),
+            _ => return Err(MoveError::GameOver),
+        }
+        if !self.global_states.in_play(m.global()) {
+            return Err(MoveError::WonBoard);
+        }
+        if let Some(b) = self.next_board {
+            if b != m.global() as u8 {
+                return Err(MoveError::WrongBoard);
+            }
+        }
+        if self.local_boards.at(m.global(), m.local()) != CellState::Empty {
+            return Err(MoveError::NotEmpty);
+        }
+
+        self.local_boards.set(m.global(), m.local(), self.next_player);
+        
+        let board_state = self.local_boards.check_winner(m.global(), self.next_player);
+        self.global_states.set(m.global(), board_state);
+        
+        if self.global_states.in_play(m.local()) {
+            self.next_board = Some(m.local() as u8);
+        } else {
+            self.next_board = None;
+        }
+        self.game_state = self.global_states.check_winner(self.next_player);
+        self.next_player = self.next_player.other();
+        return Ok(());
+    }
+
+    pub fn all_moves<'a>(&'a self) -> impl Iterator<Item = Move> + 'a {
+        MoveIterator::from_game(&self)
+    }
+
+    pub fn game_state(&self) -> GameState {
+        self.game_state
+    }
+
+    pub fn game_over(&self) -> bool {
+        match self.game_state {
+            GameState::InPlay => false,
+            _ => true,
+        }
+    }
+
+    pub fn player(&self) -> Player {
+        self.next_player
+    }
+
+    pub fn board_to_play(&self) -> Option<usize> {
+        self.next_board.map(|b| b as usize)
+    }
+
+    pub fn at(&self, board: usize, cell: usize) -> CellState {
+        self.local_boards.at(board, cell)
+    }
+
+    pub fn board_state(&self, board: usize) -> GameState {
+        self.global_states.at(board)
+    }
+
+    pub fn open_boards(&self) -> u8 {
+        (!self.global_states.donebits() & BOARD_MASK).count_ones() as u8
+    }
+}
+
+struct MoveIterator<'a> {
+    game: &'a Game,
+    board: usize,
+    mask: u32,
+    cell: usize,
+}
+
+impl<'a> MoveIterator<'a> {
+    fn from_game(game: &'a Game) -> Self {
+        let board = match game.board_to_play() {
+            Some(b) => b,
+            None => (0..9).find(|b| game.global_states.in_play(*b)).unwrap_or(9),
+        };
+        MoveIterator {
+            game: game,
+            cell: 0,
+            mask: game.local_boards.mask(board),
+            board: board,
+        }
+    }
+}
+
+impl<'a> Iterator for MoveIterator<'a> {
+    type Item = Move;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.cell >= 9 {
+                if let Some(_) = self.game.board_to_play() {
+                    return None;
+                }
+                self.board += 1;
+                if !self.game.global_states.in_play(self.board) {
+                    continue;
+                }
+                if self.board >= 9 {
+                    return None;
+                }
+                self.cell = 0;
+                self.mask = self.game.local_boards.mask(self.board);
+            }
+
+            let bit = self.mask & 1 == 0;
+            let cell = self.cell;
+            self.mask >>= 1;
+            self.cell += 1;
+            if bit {
+                return Some(Move::from_coords(self.board, cell));
+            }
+        }
+    }
+}
+
+fn codingame() {
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let opponent_row = parse_input!(inputs[0], i32);
+        let opponent_col = parse_input!(inputs[1], i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let valid_action_count = parse_input!(input_line, i32);
+        for i in 0..valid_action_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let row = parse_input!(inputs[0], i32);
+            let col = parse_input!(inputs[1], i32);
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        println!("0 0");
+    }
+}
+
+fn main() {
+
+    codingame();
+}
